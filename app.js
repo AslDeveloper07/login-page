c5796f66c74f8aeceac070a4e727dcf8643ae909
AslDeveloper07 <aslwexking777@gmail.com>
2025-05-02 22:17:16 +0500

    add

--- app.js
+++ app.js
@@ -3,23 +3,67 @@ const users = [
         email: "asilbek@gmail.com",
         password: "12345678",
         name: "Asilbek",
-        username: "Suvonov",
+        username: "@Suvonov",
         img: "./img/asilbekjon.png",
-        story: "Hayotda muvaffaqiyatga erishish uchun uch narsa kerak; diqqat, intizom va harakat!"
+        story: "Ota-onang sendan faxrlanmaguncha harakatdan tuxtama!",
+        followers: "123",
+        following: "230",
+        fullName: "Asilbek Suvonov",
+        phoneNumber: "+998 88 703 13 86",
+        company: "Sfera Academy",
+        website: "www.LoginPage",
+        awards: "5",
+        interested: "Design, Developer",
+        linkedin:"Asilbekjon777",
+        instagram:"@Asilch1k_ake",
+        blog:"@manoli_izoxlar",
+        document:"30",
+        googleEmail:"Asilbek@gmail.com",
+        location:"Uzbekistan",
+        dateBirth:"Juny-06, 2008",
+        occupation:"Junior",
+        joined:"september 2024",
+        twitter:"@Asilbek",
+        facebook:"Asilbek.uz",
+        gitHub:"@AslDeveloper07",
+        project:"12",
+        downloads:"100"
     },
     {
         email: "Azizbek@gmail.com",
         password: "12345678",
         name: "Azizbek",
-        username: 'Azizbek',
+        username: '@Azizbek',
         img: "./img/image.png",
-        story: "Hayotda muvaffaqiyatga erishish uchun uch narsa kerak; diqqat, intizom va harakat!"
+        story: "Hayotda muvaffaqiyatga erishish uchun uch narsa kerak; diqqat, intizom va harakat!",
+        followers: "300",
+        following: "303",
+        fullName: "Azizbek Suvonov",
+        phoneNumber: "+998 88 137 20 09",
+        company: "Sfera Academy",
+        website: "www.Portfolio",
+        awards: "6",
+        interested: "Design, Developer",
+        linkedin:"Azizbek1234",
+        instagram:"@Azizbek.011",
+        blog:"@developer_world",
+        document:"43",
+        googleEmail:"Azizbek@gmail.com",
+        location:"Uzbekistan",
+        dateBirth:"December - 06, 2008",
+        occupation:"Beginner",
+        joined:"octomber 2024",
+        twitter:"@Azizbek",
+        facebook:"Azizbek.uz",
+        gitHub:"@Azizbek011",
+        project:"12",
+        downloads:"100"
     },
     {
         email: "umar@gmail.com",
         password: "12345678",
         name: "Umar",
-        username: 'Umarali',
+        username: '@Umarali',
         img: "./img/developer.png",
         story: "Pul uchun barcha narsaga tayyor inson bilan hech qachon do‘stlashma rozi qilishsa seni ham sotadi."
     },
@@ -27,7 +71,7 @@ const users = [
         email: "Joniy@gmail.com",
         password: "Joniy123",
         name: "Joniy",
-        username: 'Joniy',
+        username: '@Joniy',
         img: "./img/dev.png",
         story: "Irodang kuchli bo‘lmasa, kuching senga yordam berolmaydi."
     },
@@ -41,7 +85,8 @@ let paragraph = document.querySelector('#paragraph');
 let firstName = document.querySelector('.name');
 let userName = document.querySelector('.username');
 let avatar = document.querySelector('.avatar');
-let text =document.querySelector('.bio')
+let text = document.querySelector('.bio')
+let fullNames = document.querySelector()
 
 
 let getUserInfo = JSON.parse(localStorage.getItem('user'));
